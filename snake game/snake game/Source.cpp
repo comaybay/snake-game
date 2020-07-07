e6b370ad83596db2c80e4a3b7edfbefb850f6228
nguyen thoai linh <thoailinh111@gmail.com>
2020-07-07 17:03:37 +0700

    them chức năng dừng ngay lập tức và bổ sung màu sác cho trò chơi

--- snake game/snake game/Source.cpp
+++ snake game/snake game/Source.cpp
@@ -4,6 +4,23 @@
 #include <conio.h>
 #include <stdlib.h>
 #include <time.h>
+#define RESET   "\033[0m"
+#define BLACK   "\033[30m"      /* Black */
+#define RED     "\033[31m"      /* Red */
+#define GREEN   "\033[32m"      /* Green */
+#define YELLOW  "\033[33m"      /* Yellow */
+#define BLUE    "\033[34m"      /* Blue */
+#define MAGENTA "\033[35m"      /* Magenta */
+#define CYAN    "\033[36m"      /* Cyan */
+#define WHITE   "\033[37m"      /* White */
+#define BOLDBLACK   "\033[1m\033[30m"      /* Bold Black */
+#define BOLDRED     "\033[1m\033[31m"      /* Bold Red */
+#define BOLDGREEN   "\033[1m\033[32m"      /* Bold Green */
+#define BOLDYELLOW  "\033[1m\033[33m"      /* Bold Yellow */
+#define BOLDBLUE    "\033[1m\033[34m"      /* Bold Blue */
+#define BOLDMAGENTA "\033[1m\033[35m"      /* Bold Magenta */
+#define BOLDCYAN    "\033[1m\033[36m"      /* Bold Cyan */
+#define BOLDWHITE   "\033[1m\033[37m"      /* Bold White */
 using namespace std;
 
 
@@ -44,7 +61,7 @@ void VeManHinh(int x0, int y0, int width, int height)
 
     render_str += LINE_BREAK + OFFSET_X + repeat(width / 2, WALL_TEXTURE + (string)" ");
 
-    cout << render_str;
+    cout << BLUE << render_str << RESET;
 }
 
 struct Point
@@ -134,10 +151,10 @@ void ConRan::Ve()
     for (int i = 0; i < ChieuDai - 1; i++)
     {
         gotoxy(Body[i].x, Body[i].y);
-        cout << 'o';
+        cout << GREEN<< 'o'<<RESET;
     }
     gotoxy(Head.x, Head.y);
-    cout << '+';
+    cout <<YELLOW<< '+'<<RESET;
 }
 
 void ConRan::DiChuyen()
@@ -202,7 +219,7 @@ void Food::CreateFood(int a, int b, int c, int d)
 void Food::Ve()
 {
     gotoxy(f.x, f.y);
-    cout << '*';
+    cout <<RED << '*'<<RESET;
 }
 
 
@@ -238,6 +255,7 @@ int main()
             k = _getch();
             if (k == 49) system("pause");
             if (k == 50) snake.reset(x0, y0);
+            if (k == 27) break;
         }
 
         snake.DiChuyen();
@@ -278,7 +296,7 @@ int main()
             snake.AnDiem(food.getX(), food.getY());
             food.CreateFood(x0, y0, width, height);
         }
-
+        
         Sleep(200 - level * 25);
         system("cls");
     }
